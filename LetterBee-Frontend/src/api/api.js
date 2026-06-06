db5eeb34d13de581a775ef53a69e4a49574758c6
Aditya Shee <161288779+AdityaShee12@users.noreply.github.com>
2026-06-06 12:09:38 +0530

    Update backend API in LetterBee-Frontend folder

--- LetterBee-Frontend/src/api/api.js
+++ LetterBee-Frontend/src/api/api.js
@@ -5,7 +5,7 @@ import { store } from "../app/store";
 // BASE CONFIG
 const API_URL =
     import.meta.env.VITE_API_URL ||
-    "http://localhost:8000/";
+    "https://letterbee-backend.onrender.com";
 
 const api = axios.create({
     baseURL: API_URL,
