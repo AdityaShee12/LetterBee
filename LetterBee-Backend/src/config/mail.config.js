8da67c9330a2524d895127eed4317d5c1fecc699
Aditya Shee <sheeaditya12@gmail.com>
2026-08-29 09:44:30 +0530

    Commented total code of Nodemailer

--- LetterBee-Backend/src/config/mail.config.js
+++ LetterBee-Backend/src/config/mail.config.js
@@ -1,16 +1,16 @@
-import nodemailer from "nodemailer";
+// import nodemailer from "nodemailer";
 
-const transporter = nodemailer.createTransport({
-  host: process.env.BREVO_SMTP_HOST,
-  port: Number(process.env.BREVO_SMTP_PORT),
-  secure: false,
-  auth: {
-    user: process.env.BREVO_SMTP_USER,
-    pass: process.env.BREVO_SMTP_PASSWORD,
-  },
-});
+// const transporter = nodemailer.createTransport({
+//   host: process.env.BREVO_SMTP_HOST,
+//   port: Number(process.env.BREVO_SMTP_PORT),
+//   secure: false,
+//   auth: {
+//     user: process.env.BREVO_SMTP_USER,
+//     pass: process.env.BREVO_SMTP_PASSWORD,
+//   },
+// });
 
-export { transporter };
+// export { transporter };
 
 // import nodemailer from "nodemailer";
 
